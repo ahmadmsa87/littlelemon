62be8d802a635c42ad71a2e486cd81f6d554f493
Ahmad Saleh <ahmadmsa87@gmail.com>
2023-08-01 17:57:17 +0400

    create a Swift UI project called Little Lemon

---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Ahmad Saleh on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
